11f6b1355191d9c2afe325dfb9e6a733a16f0ba3
Dragan Jović <dj47@zips.uakron.edu>
2017-02-14 02:43:12 -0500

    Update ordered_list.cpp

--- Projects/Ordered List/ordered_list.cpp
+++ Projects/Ordered List/ordered_list.cpp
@@ -4,214 +4,137 @@
 
 #include "ordered_list.hpp"
 
-#include <iostream>
 #include <cassert>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-
-Ordered_list::Ordered_list() // Set all pointers to NULL (head, current, and temp)
+Ordered_list::Ordered_list() // Set all pointers to NULL (head, current, and
+                             // temp)
 {
-    
-    head = NULL;
-    trail = NULL;
-    curr = NULL;
-    temp = NULL;
+
+  head = NULL;
+  trail = NULL;
+  curr = NULL;
+  temp = NULL;
 }
 
 Ordered_list::~Ordered_list() // Destructor
-{
+{}
 
+void Ordered_list::insert(int insertData) {
+  node *newNode = new node{insertData = insertData, newNode = NULL};
+
+  // empty list
+  if (head == NULL) {
+    head = newNode;
+
+  } else {
+    node *curr = head;
+    node *trail = NULL;
+
+    // Traverse list to find insert location
+    while (curr != NULL) {
+      if (curr->data > newNode->data) {
+        break;
+      } else {
+        trail = curr;
+        curr = curr->next;
+      }
+    }
+
+    // insert at head
+    if (curr == head) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+
+      // insert after the head
+      newNode->next = curr;
+      trail->next = newNode;
+    }
+  }
 }
 
-void Ordered_list::insert(int insertData)
+void Ordered_list::remove(
+    int removeData) // Pass in data that will be stored in "removeData"
 {
-    node* newNode = new node
-    {
-        insertData = insertData,
-        newNode = NULL
-    };
-    
-    // empty list
-    if (head == NULL)
-    {
-        head = newNode;
-        
-    }
-    else
-    {
-        node* curr = head;
-        node* trail = NULL;
-        
-        // Traverse list to find insert location
-        while(curr != NULL)
-        {
-            if(curr->data > newNode->data)
-            {
-                break;
-            }
-            else
-            {
-                trail = curr;
-                curr = curr->next;
-            }
-        }
-        
-        // insert at head
-        if(curr == head)
-        {
-            newNode->next = head;
-            head = newNode;
-        }
-        else
-        {
-        
-        // insert after the head
-            newNode->next = curr;
-            trail->next = newNode;
-        }
-        
-    }
-    
+  node *deleteptr = NULL; // Create node pointer
+  temp = head;
+  curr = head;
+
+  while (curr != NULL && curr->data != removeData) {
+    temp = curr;
+    curr = curr->next;
+  }
+  // Check to see if current pointer is pointing at anything
+  if (curr == NULL) {
+    // cout << removeData << " was not found in the list\n";
+    delete deleteptr; // Takes care of memory leaks
+  } else              // Node was found, and so it will be deleted
+  {
+    deleteptr = curr;
+    curr = curr->next; // Advances our curr pointer to "next" node
+    temp->next = curr;
+    delete deleteptr;
+    // cout << "The value " << removeData << " was deleted\n";
+  }
 }
 
+void Ordered_list::display() {
+  curr = head;
+  while (curr != NULL) {
+    cout << "[" << curr->data << "]";
+    curr = curr->next;
+  }
 
-void Ordered_list::remove(int removeData) // Pass in data that will be stored in "removeData"
-{
-    node* deleteptr = NULL; // Create node pointer
+  cout << endl;
+}
+
+void Ordered_list::search(int searchData) {
+  node *searchptr = NULL;
+  temp = head;
+  curr = head;
+
+  while (curr != NULL && curr->data != searchData) {
+    temp = curr;
+    curr = curr->next;
+  }
+  if (curr != NULL) {
+    searchptr = curr;
+    curr = curr->next;
+    cout << "-" << searchptr << " Found";
+  } else {
+    cout << "N/A";
+  }
+}
+
+void Ordered_list::clear() {
+  if (!head) {
+    return;
+  }
+
+  node *temp = head;
+  while (temp) {
+    head = temp->next;
+    delete temp;
     temp = head;
-    curr = head;
-    
-    while(curr != NULL && curr->data != removeData)
-    {
-        temp = curr;
-        curr = curr->next;
-    }
-    // Check to see if current pointer is pointing at anything
-    if(curr == NULL)
-    {
-        // cout << removeData << " was not found in the list\n";
-        delete deleteptr; // Takes care of memory leaks
-    }
-    else // Node was found, and so it will be deleted
-    {
-        deleteptr = curr;
-        curr = curr->next; // Advances our curr pointer to "next" node
-        temp->next = curr;
-        delete deleteptr;
-        // cout << "The value " << removeData << " was deleted\n";
-    }
+  }
 }
 
+bool Ordered_list::is_empty() { return count() == 0; }
 
-void Ordered_list::display()
-{
-    curr = head;
-    while (curr != NULL)
-    {
-        cout << "[" << curr->data << "]";
-        curr = curr->next;
-    }
-    
-    cout <<  endl;
+int Ordered_list::count() {
+  node *curr = head;
+  curr = head;
+  int _count = 0;
+
+  while (curr != NULL) {
+    _count++;
+    curr = curr->next;
+  }
+  return _count;
 }
-
-void Ordered_list::search(int searchData)
-{
-    node* searchptr = NULL;
-    temp = head;
-    curr = head;
-    
-    while (curr != NULL && curr->data != searchData)
-    {
-        temp = curr;
-        curr = curr->next;
-    }
-    if(curr != NULL)
-    {
-        searchptr = curr;
-        curr = curr->next;
-        cout << "-" << searchptr << " Found";
-    }
-    else
-    {
-        cout << "N/A";
-        
-    }
-}
-
-void Ordered_list::clear()
-{
-    if (!head)
-    {
-        return;
-    }
-    
-    node* temp = head;
-    while(temp)
-    {
-        head = temp->next;
-        delete temp;
-        temp = head;
-    }
-}
-
-bool Ordered_list::is_empty()
-{
-    return count() == 0;
-}
-
-int Ordered_list::count()
-{
-    node* curr = head;
-    curr = head;
-    int _count = 0;
-    
-    while( curr != NULL)
-    {
-        _count++;
-        curr = curr->next;
-    }
-    return _count;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
