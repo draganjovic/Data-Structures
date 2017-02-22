29c250fa67e8f2d3ee6ad888cc74f0ee328157dd
Dragan Jović <dj47@zips.uakron.edu>
2017-02-22 12:38:44 -0500

    Update palindrome.cpp

--- Projects/Palindrome/palindrome.cpp
+++ Projects/Palindrome/palindrome.cpp
@@ -13,51 +13,78 @@
 
 using namespace std;
 
-
-
-int main()
+class Palindrome
 {
-    // Variables
-    string line;        // one output line by user
-    int mismatch = 0;  // spots that do not match
-    char letter;      // character from string
-    int length;       // length of string
 
+  public:
+    void check_string();
     bool isPalindrome(string input);
+};
 
+  void Palindrome::check_string()
+  {
+      string line;        // one output line by user
 
-    Stack myStack;    // Stack declaration
-    Queue myQueue;   // Queue declaration
-
-
-    // While a string is entered on stand input and is not "q"
-    do
-    {
-        cout << "Your input: " << endl;
-        cin >> line;
-        if (isPalindrome(line))
-          cout << "This is a Palindrome" << endl;
-        else
-          cout << "This is not a Palindrome" << endl;
-    }
-    while ((line.length() != 0) && letter != 'q');
-
-      // using recursion. add each character to both stack and queue
-      if (!myStack.full() && !myQueue.full())
+      // While a string is entered on stand input and is not "q"
+      do
       {
-        return (myStack.push(), myQueue.enqueue());
+          cout << "Your input: " << endl;
+          cin >> line;
+          if (isPalindrome(line))
+            cout << "This is a Palindrome" << endl;
+          else
+            cout << "This is not a Palindrome" << endl;
       }
+      while ((line.length() != 0));
+  }
+
+  bool Palindrome::isPalindrome(string input)
+  {
+      Stack my_stack;
+      Queue my_queue;
+      char letter;  // one char from input string
+      int mismatch = 0; // number of spots that mismatched
+      int i;  // Index for input string
+
+      for (i = 0; i < input.length(); i++)
+      {
+        letter = input.at(i);
+        if (std::isalpha(letter))
+        {
+            my_queue.enqueue(letter);
+            my_stack.push(letter);
+        }
+      }
+      while (!my_queue.empty())
+      {
+          if (my_queue.dequeue() == my_stack.pop())
+          {
+              cout << "The characters are not equal." << endl;
+              mismatch++;
+          }
+      }
+      return (mismatch == 0);
+}
+
+
+        // using recursion. add each character to both stack and queue
+
 
     // using recursion, pop a character from stack and dequeue character from queue
-    else (myStack.full() && myQueue.full())
-    {
-      return (myStack.pop(), myQueue.dequeue());
-    }
+
 
     // if characters are not equal, output the index of the missmatch followed by std::endl
 
 
     // recursively empty the stack and queue
-    
+
 
     // if both stack and queue are empty, output -1 followed by std::endl
+
+    int main()
+    {
+      void check_string();
+      bool isPalindrome('anna');
+
+      return 0;
+    }
